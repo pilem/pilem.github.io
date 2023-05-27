b9f199de4edaafa35ab771e7879c9ab46c5418d2
Pierre Lemay <pierre@e560.pilem.info>
2023-05-27 11:00:26 -0400

    Ajout de offPIM

---
+++ pim/precache-manifest.621b923b64c3d8ec3ae3c47836f3d005.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0a0d8f5f173be15a67aa084db94fe6",
+    "url": "fonts/materialdesignicons-webfont.2d0a0d8f.eot"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "fonts/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "fonts/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "f51112347be6b44f9ef46151a971430d",
+    "url": "fonts/materialdesignicons-webfont.f5111234.ttf"
+  },
+  {
+    "revision": "dc15d4bb9602fbe39c335eb1c2a39dc7",
+    "url": "icon.png"
+  },
+  {
+    "revision": "ddeea75c2a9b834698d7eb919f60b2c1",
+    "url": "index.html"
+  },
+  {
+    "revision": "1f5e83c40d9db70fe071",
+    "url": "js/app.e69da6cf.js"
+  },
+  {
+    "revision": "a999a8aeee7ef2b2300e",
+    "url": "js/chunk-vendors.6d90b951.js"
+  },
+  {
+    "revision": "fac703cee17030e68db3",
+    "url": "js/contacts.5241d8c5.js"
+  },
+  {
+    "revision": "8ec05b662ccca4bb2f9c",
+    "url": "js/contacts~finances~forms~inventory~logbook~messages~notes~tasks.5dd24aa6.js"
+  },
+  {
+    "revision": "baed0fe7af0f51f1594e",
+    "url": "js/contacts~finances~inventory~messages~notes~tasks.df99fe5d.js"
+  },
+  {
+    "revision": "1a00a3f93248af5bc120",
+    "url": "js/contacts~forms~logbook~tasks.3ba623bf.js"
+  },
+  {
+    "revision": "3b5c097dfaf105a2a5fa",
+    "url": "js/dashboard.fc31fa42.js"
+  },
+  {
+    "revision": "e7e5304158d2bc034609",
+    "url": "js/finances.b82486ae.js"
+  },
+  {
+    "revision": "fe705307cf00ddce39cd",
+    "url": "js/finances~inventory~tasks.712b9f00.js"
+  },
+  {
+    "revision": "c32a59d08a42cc28c5fb",
+    "url": "js/forms.cfd43b83.js"
+  },
+  {
+    "revision": "cb759062fae0ddf492ba",
+    "url": "js/forms~logbook.53c9cba0.js"
+  },
+  {
+    "revision": "8a1d3cfde8a02c807875",
+    "url": "js/inventory.dfcb27c1.js"
+  },
+  {
+    "revision": "a9e245cc9951a058cce2",
+    "url": "js/logbook.eebe23d6.js"
+  },
+  {
+    "revision": "11a6066693d04109c42f",
+    "url": "js/maps.58aaff51.js"
+  },
+  {
+    "revision": "1e4dcc051905b73a48d7",
+    "url": "js/messages.02baa9ae.js"
+  },
+  {
+    "revision": "ae54ec51f457b9f8b7b4",
+    "url": "js/notes.a1ebab2c.js"
+  },
+  {
+    "revision": "fc296ccdf0140808b9c0",
+    "url": "js/search.8c8b3097.js"
+  },
+  {
+    "revision": "33cda02c76aa9252ffd7",
+    "url": "js/settings.5df5134a.js"
+  },
+  {
+    "revision": "e10976f6c355d17db440",
+    "url": "js/setup.be960464.js"
+  },
+  {
+    "revision": "5992db0f3110e7b3360a",
+    "url": "js/tasks.27899148.js"
+  },
+  {
+    "revision": "6108fc2ccd8af1673cde",
+    "url": "js/themes.d5fd2fb8.js"
+  },
+  {
+    "revision": "9caacb4dce8e56fb3ec8",
+    "url": "js/tools.10d4397e.js"
+  },
+  {
+    "revision": "417a5f9b5f919ab10cf3662882e9eaf8",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
